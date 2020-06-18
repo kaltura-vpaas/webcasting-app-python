5309bb972743db4d51aafb92fa1dd369330dbbd3
Avital Tzubeli <avital.tzubeli@kaltura.com>
2020-06-18 23:09:22 +0300

    edit config

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
-partner_id
-admin_secret
-admin_email
-service_url
-uiconf_id
+partner_id =
+admin_secret =
+admin_email =
+service_url =
+uiconf_id =
 
-user_id
-moderator_user
+user_id =
+moderator_user =
 
-app_name
-app_id
-app_domain
-
-
+app_name =
+app_id =
+app_domain =
